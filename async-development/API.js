d4d27d92ed540a10c3b104c3eef621bf53cb269e
Nikita Stepanov <gulivon@mail.ru>
2019-04-18 18:22:54 +0300

    fix: fix html and js file

--- async-development/API.js
+++ async-development/API.js
@@ -1,8 +1,7 @@
-let btn = document.getElementById('btn');
 let form = document.getElementById('form');
+let city = document.getElementById('input');
 form.addEventListener('submit', function() {
-    let city = document.getElementById('input').value;
-    let url = `http://api.apixu.com/v1/forecast.json?key=13fc76b820274314b17173821191302&q=${city}`;
+    let url = `http://api.apixu.com/v1/forecast.json?key=13fc76b820274314b17173821191302&q=${city.value}`;
     fetch(url)
     .then(r => r.json())
     .then(data => {
